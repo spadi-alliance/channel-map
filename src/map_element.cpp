5ff0581f8166918c1e9e56c0e52a9ad3ca97008a
Shuhei Hayakawa <shuhei.hayakawa.c8@tohoku.ac.jp>
2024-06-25 23:45:34 +0900

    Remove map_element.cpp

--- src/map_element.cpp
+++
@@ -1,9 +0,0 @@
-#include "map_element.hpp"
-
-namespace cmap {
-
-// std::map<std::string, elnum_t> MapElement::m_str2int_table;
-// std::vector<std::string> MapElement::m_int2str_table;
-// std::string MapElement::m_null_str;
-
-}
